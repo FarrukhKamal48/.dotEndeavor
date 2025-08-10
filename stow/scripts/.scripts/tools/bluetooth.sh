81ce3002c3d9c646932c27da78ce75937cd91dc2
FarrukhKamal48 <farrukhkamal429@gmail.com>
2025-08-10 18:49:39 +0500

    stow: scrips: added dunst notification whenever bluetooth is powered ON/OFF

--- stow/scripts/.scripts/tools/bluetooth.sh
+++ stow/scripts/.scripts/tools/bluetooth.sh
@@ -4,10 +4,12 @@ case $1 in
     
     power_on)
     rfkill unblock bluetooth && sleep 1s && bluetoothctl power on
+    dunstify -a "bluetooth.sh" -u low -i bluetooth -r 2595 "Bluetooth" "Power On"
     ;;
     
     power_off)
     bluetoothctl power off && sleep 1s && rfkill block bluetooth
+    dunstify -a "bluetooth.sh" -u low -i bluetooth-off -r 2595 "Bluetooth" "Power Off"
     ;;
     
     list_connected)
