289022854cc5b0a92a9aabf330bf4a028fff0479
FarrukhKamal48 <farrukhkamal429@gmail.com>
2025-08-10 18:12:36 +0500

    stow: polybar: heavily reworked bluetooth module custom script

--- stow/polybar/.config/polybar/custom/bluetooth.sh
+++ stow/polybar/.config/polybar/custom/bluetooth.sh
@@ -1,56 +1,77 @@
 #!/bin/bash
 
-off_glyph="󰂲"
-off_text="OFF"
+GLYPH_FONT=%{T7}
+TEXT_FONT=%{T1}
+GLYPH_COLOR=%{F#eba0ac}
+TEXT_COLOR=%{F#89dceb}
 
-on_glyph="󰂯"
-on_text="ON"
+POWER_OFF_GLYPH="󰂲"
+POWER_ON_GLYPH="󰂯"
 
-connected_glyph="󰋋"
+GLYPH_SEPERATOR=%{O5}
+DEVICE_SEPERATOR=%{O10}
+
+declare -A devices
+devices["84:AC:60:31:89:58"]="󰋋 PEATS"
+
+POWERED=$(bluetoothctl show | grep -c "Powered: yes")
 
 case $1 in
     
     power_toggle)
-    powered=$(bluetoothctl show | grep 'Powered:' | sed 's/.*: //g')
-    if [[ "$powered" == "yes" ]]; then
-        ~/.scripts/tools/bluetooth.sh power_off
-    else
-        ~/.scripts/tools/bluetooth.sh power_on
-    fi
-    ;;
+        if (( ${POWERED} )); then
+            ~/.scripts/tools/bluetooth.sh power_off
+        else
+            ~/.scripts/tools/bluetooth.sh power_on
+        fi
+        ;;
 
     format)
-    powered=$(bluetoothctl show | grep 'Powered:' | sed 's/.*: //g')
-    connected=$(bluetoothctl info | grep 'Connected:' | sed 's/.*: //g')
+        OUTPUT=${GLYPH_FONT}${GLYPH_COLOR}
+        
+        # bluetooth controller status
+        if (( ${POWERED} )); then
+            OUTPUT+=${POWER_ON_GLYPH}
+            OUTPUT+=${GLYPH_SEPERATOR}
+            OUTPUT+=${TEXT_FONT}${TEXT_COLOR}
+            OUTPUT+="ON" 
+        else
+            OUTPUT+=${POWER_OFF_GLYPH}
+            OUTPUT+=${GLYPH_SEPERATOR}
+            OUTPUT+=${TEXT_FONT}${GLYPH_COLOR}
+            OUTPUT+="OFF" 
+        fi
 
-    glyph_color_normal="%{F#eba0ac}"
-    glyph_color_alert="%{F#89dceb}"
-    text_color_normal="%{F#eba0ac}"
+        # bluetooth devices
+        for dev_adr in ${!devices[@]}; do
+            dev_info=$(bluetoothctl info ${dev_adr})
+            connected=$(echo ${dev_info} | grep -c "Connected: yes")
 
-    icon=""
-    icon_color="${glyph_color_normal}"
-    text=""
-    text_color="${text_color_normal}"
+            dev_texts=(${devices["${dev_adr}"]})
 
-    if [[ "$powered" == "yes" ]]; then
-        icon=${on_glyph}
-        icon_color="${glyph_color_normal}"
-        text=${on_text}
-    else
-        icon=${off_glyph}
-        icon_color="${glyph_color_alert}"
-        text=${off_text}
-    fi
+            OUTPUT+=${DEVICE_SEPERATOR}
+            OUTPUT+=${GLYPH_FONT}${GLYPH_COLOR}
 
-    if [[ "$connected" == "yes" ]]; then
-        icon=${connected_glyph}
-        icon_color="${glyph_color_normal}"
-        device=$(bluetoothctl info | grep 'Alias' | sed 's/.*: //g' | awk '{print $1}')
-        battery=$(bluetoothctl info | grep 'Battery Percentage' | sed 's/.*(//g' | cut -d ')' -f 1)'%'
-        text="${device} ${battery}"
-    fi
+            # add glyph for device
+            OUTPUT+=${dev_texts[0]}
+            unset dev_texts[0]
 
-    echo "%{T7}${icon_color}${icon}%{T1} ${text_color}${text}"
+            # add device name
+            OUTPUT+=${TEXT_FONT}
+            OUTPUT+=${GLYPH_SEPERATOR}
+            OUTPUT+=${GLYPH_SEPERATOR}
+            
+            if (( ${connected} )); then
+                OUTPUT+=${TEXT_COLOR}
+                OUTPUT+="${dev_texts[@]}"
+            else
+                OUTPUT+="OFF"
+            fi
+            
+        done
+        
+        printf '%s\n' "${OUTPUT}"
     ;;
 
 esac
+
