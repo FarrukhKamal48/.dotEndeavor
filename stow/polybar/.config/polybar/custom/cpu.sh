667e3eea03ab3bc4d278a1e623c3be8fe14abc43
FarrukhKamal48 <farrukhkamal429@gmail.com>
2025-08-26 13:09:11 +0500

    stow; polybar; added cpu utilization percentages

--- stow/polybar/.config/polybar/custom/cpu.sh
+++ stow/polybar/.config/polybar/custom/cpu.sh
@@ -2,10 +2,15 @@
 
 format=0
 STATUS_FILE="/mnt/media/cpu.polybar"
-FORMAT_COUNT=4
+CPU_TIMES_LAST_FILE="/mnt/media/cpu-time-totals-last.polybar"
+FORMAT_COUNT=3
 
 if [[ ! -s ${STATUS_FILE} ]]; then
     echo "format=${format}" > ${STATUS_FILE}
+fi
+curr_cpu_time_info=($(cat /proc/stat | grep "cpu" | sed 's/cpu..//g'))
+if [[ ! -s ${CPU_TIMES_LAST_FILE} ]]; then
+    echo "${curr_cpu_time_info[@]}" > ${CPU_TIMES_LAST_FILE}
 fi
 
 BIG_FONT=%{T5}
@@ -34,7 +39,7 @@ while [[ ${#@} -gt 0 ]]; do
             format=$(cat ${STATUS_FILE} | grep -Po "[0-9]")
             ;;
 
-        --format|-t)
+        --format|-f)
             shift
             format=${1}
             ;;
@@ -49,7 +54,22 @@ while [[ ${#@} -gt 0 ]]; do
     shift
 done
 
-usages=(0 1 2 3 4)
+last_cpu_time_info=($(cat ${CPU_TIMES_LAST_FILE}))
+
+for cpu in {0..16}; do
+    total_prev=0
+    total_curr=0
+    for col in {0..9}; do
+        total_prev=$(( ${total_prev} + ${last_cpu_time_info[(( 10 * ${cpu} + ${col} ))]} ))
+        total_curr=$(( ${total_curr} + ${curr_cpu_time_info[(( 10 * ${cpu} + ${col} ))]} ))
+    done
+    idle_diff=$(( ${curr_cpu_time_info[(( 10 * ${cpu} + 3 ))]} - ${last_cpu_time_info[(( 10 * ${cpu} + 3 ))]} ))
+    total_diff=$(( ${total_curr} - ${total_prev} ))
+    
+    usages[${cpu}]=$(echo "scale=0; (( (${total_diff} - ${idle_diff}) * 100/${total_diff} ))" | bc -l)
+done
+
+
 temps=($(sensors -j \
     | jq '.[] | select(.Adapter=="ISA adapter") | .[] | select(type=="object") | to_entries[] | select(.key|test("^temp.*_input$")) | .value' \
     | xargs printf '%.0f\n'))
@@ -67,13 +87,13 @@ case ${format} in
         OUTPUT+=${usages[0]}${USAGE_UNIT}
         ;;
 
-    1|2|3)
+    1|2)
         OUTPUT+=${BIG_FONT}${CPU_TEMP_GLYPH}${NORMAL_FONT}
         OUTPUT+=${GLYPH_SEPERATOR}
 
         OUTPUT+=${temps[0]}${GLYPH_SEPERATOR}
         unset temps[0]
-        if [[ ${format} -eq 2 ]] || [[ ${format} -eq 3 ]]; then
+        if [[ ${format} -eq 2 ]]; then
             for T in ${temps[@]}; do
                 OUTPUT+=${T}${GLYPH_SEPERATOR}
             done
@@ -87,11 +107,9 @@ case ${format} in
         
         OUTPUT+=${usages[0]}${GLYPH_SEPERATOR}
         unset usages[0]
-        if [[ ${format} -eq 1 ]] || [[ ${format} -eq 3 ]]; then
-            for U in ${usages[@]}; do
-                OUTPUT+=${U}${GLYPH_SEPERATOR}
-            done
-        fi
+        for U in ${usages[@]}; do
+            OUTPUT+=${U}${GLYPH_SEPERATOR}
+        done
         OUTPUT+=${USAGE_UNIT}
         
         ;;
@@ -105,3 +123,4 @@ esac
 printf '%s\n' ${OUTPUT}
 
 echo "format=${format}" > ${STATUS_FILE}
+echo "${curr_cpu_time_info[@]}" > ${CPU_TIMES_LAST_FILE}
