b0aa38479f41ddb2d097675ae8a7cb69dd7cc84b
FarrukhKamal48 <farrukhkamal429@gmail.com>
2025-08-26 05:25:35 +0500

    stow; polybar; changed memory icon

--- stow/polybar/.config/polybar/custom/memory.sh
+++ stow/polybar/.config/polybar/custom/memory.sh
@@ -14,9 +14,9 @@ NORMAL_FONT=%{T1}
 THRESH=("3.0" "7.0" "16")
 COLOR=("#a6e3a1" "#fab387" "#f38ba8")
 
-GLYPH_SEPERATOR=%{O8}
+GLYPH_SEPERATOR=%{O10}
 UNIT=%{O1}g
-GLYPH=""
+GLYPH=""
 
 TEXT_COLOR="%{F#a6e3a1}"
 declare glyph_color
