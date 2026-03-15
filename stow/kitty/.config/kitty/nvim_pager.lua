cd0a0df68e0348816ff32f6d2ba3bc6d2f760de0
FarrukhKamal48 <farrukhkamal429@gmail.com>
2026-03-15 20:17:15 +0500

    stow; kitty; added escape keybinds to nvim_pager

--- stow/kitty/.config/kitty/nvim_pager.lua
+++ stow/kitty/.config/kitty/nvim_pager.lua
@@ -1,4 +1,6 @@
 dofile "/home/infidel/.config/nvim/lua/options.lua"
 dofile "/home/infidel/.config/nvim/lua/keymaps.lua"
 
+vim.keymap.set("i", "jk", "<Esc>", {desc = "exit in insert mode", silent = false})
 vim.keymap.set("n", "q", "<cmd>q!<CR>", {desc = "close pager", silent = false})
+vim.keymap.set("n", "<leader>E", "<cmd>:Lexplore<CR>", {desc = "open Lexplore", silent = false})
